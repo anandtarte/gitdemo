05394efc8226d5dedd34740d462ce657df47e579
anandtarte10 <anandtarte10@gmail.com>
2023-09-21 01:23:47 +0530

    ma,filter,reducep

---
+++ script.js
@@ -0,0 +1,64 @@
+// let u1=abc(2,3);
+// let u2=abc(3,4);
+// let u3=abc(4,5);
+
+// console.log(u1,u2,u3);
+
+// function abc(a,b){
+//     let c=a+b;
+//     return c;
+// }
+
+// function Hello(){
+//     console.log("Sayhello called")
+
+//     function something(){
+//         console.log("something")
+//     }
+//     something();
+// }
+// function Hi(hello1){
+//     hello1();
+// }
+// Hi(Hello);
+// let xyz=()=>{
+//     console.log("anand");
+// }
+// xyz();
+
+let arr=[1,2,3,4];
+
+
+// arr.push(10);
+
+// arr.pop();
+// console.log(arr)
+// console.log(arr.length);
+
+
+// arr.shift();
+// arr.unshift("anand");
+// console.log(arr);
+
+
+arr.forEach((item)=>{
+    console.log("foreach function",item);
+});
+// let transformArr=arr.map((item)=>{
+//     if(item>2)
+//         return item * 2;
+// })
+
+// let transformArr2=arr.filter((item)=>{
+//     return item>2;
+// })
+
+// console.log(transformArr);
+// console.log(transformArr2);
+
+// let transformArr3=arr.reduce((a,item)=>{
+//     return a=a+item;
+// },0);
+
+// console.log(transformArr3);
+
