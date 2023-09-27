55a2fbdb1e4ab8e804ae00b64f408b36803b23bb
anandtarte10 <anandtarte10@gmail.com>
2023-09-27 21:08:52 +0530

    constructor

---
+++ constructors/obj1.js
@@ -0,0 +1,8 @@
+function Constructor(name,email,rno){
+    this.name=name;
+    this.email=email;
+    this.rno=rno;
+}
+
+let student1=new Constructor("anand","anandtarte@",51);
+console.log(student1);
